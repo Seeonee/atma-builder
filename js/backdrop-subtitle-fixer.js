ce28e965c79255f7418972a34e6856863f525976
Seeonee <Seeonee@users.noreply.github.com>
2023-08-24 20:39:36 -0400

    Adding support for multiline stage names to display well on backdrop cards.

---
+++ js/backdrop-subtitle-fixer.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function ($) {
+
+
+    var updateBackdropSubtitle = function(card) {
+        var title = card.find('.title .content')[0];
+        var subtitle = card.find('.subtitle');
+        var range = document.createRange();
+        range.setStart(title, 0);
+        range.setEnd(title, 1);
+        var lines = range.getClientRects().length - 1;
+        if (lines > 1) {
+            subtitle.addClass('lowered');
+        } else {
+            subtitle.removeClass('lowered');
+        }
+    }
+
+
+    cardRefresher.register(function(card) {
+        if (card.hasClass('backdrop')) {
+            card.find('.title .content').focusout(function (e) {
+                updateBackdropSubtitle(card);
+            });
+            updateBackdropSubtitle(card);
+        }
+    });
+
+ 
+});
